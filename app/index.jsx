561ef129fa58bce8bbc972f263715ec154874109
StevenMODIMO <stevepetes26@gmail.com>
2025-12-16 23:44:58 +0300

    learning react native

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+        React Native Starter App.
+      </Text>
+      <Pressable
+        style={{ padding: 10, borderRadius: 10, backgroundColor: "#e5e5e5" }}
+      >
+        <Text style={{ color: "white" }}>Hello</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
